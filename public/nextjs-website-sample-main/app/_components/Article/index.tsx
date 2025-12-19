f9666216c8eefebb67dc1d584ff361efd5a788b4
itc-n25005 <n25005@std.it-college.ac.jp>
2025-12-19 09:28:27 +0900

    commit

--- public/nextjs-website-sample-main/app/_components/Article/index.tsx
+++ public/nextjs-website-sample-main/app/_components/Article/index.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import type { News } from '@/app/_libs/microcms';
-import Date from '../Date';
-import Category from '../Category';
-import styles from './index.module.css';
+import Link from "next/link";
+import Image from "next/image";
+import type { News } from "@/app/_libs/microcms";
+import Date from "../Date";
+import Category from "../Category";
+import styles from "./index.module.css";
 
 type Props = {
   data: News;
@@ -12,8 +12,6 @@ type Props = {
 export default function Article({ data }: Props) {
   return (
     <main>
-      <h1 className={styles.title}>{data.title}</h1>
-      <p className={styles.description}>{data.description}</p>
       <div className={styles.meta}>
         <Link
           href={`/news/category/${data.category.id}`}
